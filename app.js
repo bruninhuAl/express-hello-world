cfaf8f38c9d81649dcf508dbfe650aae6b2fbdfd
bruninhuAl <brunoacristo13@gmail.com>
2024-11-29 16:05:44 -0400

    comit criando meu nome

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+  console.log("Just got a request!")
+  res.send('Meu nome é Bruno Almeida de Cristo')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
